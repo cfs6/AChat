21d774dd183fe41c4665def404e220a79354f266
cfsong <2766607313@qq.com>
2021-04-20 14:25:26 +0800

    achat

--- CChat/base/version.h
+++
@@ -1,4 +0,0 @@
-#ifndef __VERSION_H__
-#define __VERSION_H__
-#define VERSION "1.0"
-#endif
--- CChat/libsecurity/src/md5.h
+++
@@ -1,5 +0,0 @@
-
-#ifndef _MD5FILE_H_
-#define _MD5FILE_H_
-void MD5_Calculate (const char* pContent, unsigned int nLen,char* md5);
-#endif
